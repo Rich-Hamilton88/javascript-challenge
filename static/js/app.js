f9b62e96f60c13a06fe0909808e540f9e841d14e
Rich-Hamilton88 <53978255+Rich-Hamilton88@users.noreply.github.com>
2019-12-19 11:10:07 -0500

    Homework Part 1

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,58 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// function that will display UFO sightings
+
+function tableDisplay(ufosightings) {
+    var tbody = d3.select("tbody");
+    ufosightings.forEach((ufoRecord) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoRecord).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.html(value);
+        });
+    });
+};
+
+// function that will clear the table data for new data
+function deleteTbody() {
+    d3.select("tbody")
+        .selectAll("tr").remove()
+        .selectAll("td").remove();
+};
+
+//Initial display of all UFO sightings
+console.log(tableData);
+tableDisplay(tableData);
+
+//Here the filter button is created
+var button = d3.select("#filter-btn");
+
+//Filtering the database and displaying the content
+
+button.on("click", function(event) {
+    d3.event.preventDefault();
+    deleteTbody();
+    var dateInput = d3.select("#datetime").property("value");
+
+    if (dateInput.trim() === "") {
+        //This will display the entire database if the date field has no date
+        var filteredData = tableData;
+
+    } else {
+        //otherwise will display the filtered dataset
+        var filteredData = tableData.filter(ufosightings =>
+            ufosightings.datatime === dateInput.trim());
+    };
+
+    //Here we will display a message that will advise if no record can be found
+    if (filteredData.length == 0) {
+        d3.select("tbody")
+            .append("tr")
+            .append("td")
+                .attr("colspan", 7)
+                .html("<h3>Sorry, No Records could be found.</h3>");
+    };
+    console.log(filteredData);
+    tableDisplay(filteredData);
+})
